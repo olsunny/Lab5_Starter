d4688a19923cd0e9fb0c3831f42ffcb14e92c8dd
olsunny <160102529+olsunny@users.noreply.github.com>
2026-05-05 10:59:36 -0700

    added default option and attributes for language and name of data

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,6 +3,7 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
+
 const voiceSelect = document.querySelector('#voice-select');
 const textarea   = document.querySelector('#text-to-speak');
 const talkButton = document.querySelector('#explore button');
@@ -13,27 +14,31 @@ const synth = window.speechSynthesis;
 // --- Load Voices ---
 function populateVoices() {
   const voices = synth.getVoices();
-  if (voices.length === 0) return;
  
-  voiceSelect.innerHTML = '';
- 
-  voices.forEach((voice, index) => {
+  for (voice of voices) {
     const option = document.createElement('option');
     option.value = index;
     option.textContent = `${voice.name} (${voice.lang})`;
+    if (voice.default) {
+      option.textContent += " — DEFAULT";
+    }
+    option.setAttribute("data-lang", voice.lang);
+    option.setAttribute("data-name", voice.name);
     voiceSelect.appendChild(option);
-  });
+  }
 }
  
 populateVoices();
-synth.addEventListener('voiceschanged', populateVoices);
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoices;
+}
  
 // --- Press to Talk ---
 talkButton.addEventListener('click', () => {
   const text = textarea.value.trim();
   if (!text) return;
  
-  // Cancel any speech already in progress
+  // Cancel any speech in progress
   synth.cancel();
  
   const utterance = new SpeechSynthesisUtterance(text);
@@ -54,5 +59,7 @@ talkButton.addEventListener('click', () => {
     faceImage.alt = 'Smiling face';
   });
  
-  synth.speak(utterance);});
+  synth.speak(utterance);
+});
+
 }
